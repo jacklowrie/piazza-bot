6a72786367c64749f3be05e141428236393223d4
Jack Lowrie <jack@4thwall.io>
2022-10-05 13:31:22 -0400

    working v1

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 import os
 from dotenv import load_dotenv
+
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+import re
 
 # Make the app
 load_dotenv()
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+url = "https://piazza.com/class/%s/post/" % os.environ.get("COURSE_ID")
 
 # Listens to incoming messages that contain "hello"
 # To learn available listener arguments,
 # visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
-@app.message("hello")
-def message_hello(message, say):
+@app.message(re.compile("([^@]\d*)"))
+# @app.message(re.compile("(hi|hello|hey)"))
+def message_hello(say, context):
+    number = context['matches'][0]
+    post_url = url + number
     # say() sends a message to the channel where the event was triggered
-    say(f"Hey there <@{message['user']}>!")
-
-
-
-
+    say(f"{post_url}")
 
 
 # Run the app
