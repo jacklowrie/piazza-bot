513c4ffa6d42e310e2a427b95ea0862e8fd837a7
Jack Lowrie <jack@4thwall.io>
2022-10-05 14:03:33 -0400

    can handle multiple posts in one message

--- app.py
+++ app.py
@@ -17,10 +17,11 @@ url = "https://piazza.com/class/%s/post/" % os.environ.get("COURSE_ID")
 @app.message(re.compile("(@\d*)"))
 # @app.message(re.compile("(hi|hello|hey)"))
 def message_hello(say, context):
-    number = context['matches'][0].replace('@','')
-    post_url = url + number
-    # say() sends a message to the channel where the event was triggered
-    say(f"The number is {number}. the url is: {post_url}")
+    for match in context['matches']:
+        number = match.replace('@','')
+        post_url = url + number
+        # say() sends a message to the channel where the event was triggered
+        say(f"The number is {number}. the url is: {post_url}")
 
 
 # Run the app
