85534390a863be611800b6abe49b2696c8272e3e
Jack Lowrie <jack@4thwall.io>
2022-10-05 11:43:59 -0400

    hello, world!

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-load_dotenv();
+# Make the app
+load_dotenv()
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# Listens to incoming messages that contain "hello"
+# To learn available listener arguments,
+# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+
+
+
+
+
+# Run the app
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
