53625c80dfe131ba61aa2f04cdbbc26267ab4b6a
Jack Lowrie <jack@4thwall.io>
2022-10-15 18:16:41 -0400

    remove course id from example .eng

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from sys import exit
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+import logging
 
 import re
 from tinydb import TinyDB, Query
 
-
+logging.basicConfig(level=logging.ERROR)
 # Make the app
 load_dotenv()
 
@@ -17,23 +18,33 @@ app = App(token = os.environ.get("SLACK_BOT_TOKEN"))
 db = TinyDB('data/db.json')
 Course = Query()
 
-base_url = "https://piazza.com/class/%s" % os.environ.get("COURSE_ID")
-posts_url = f"{base_url}/post/" 
+base_url = "https://piazza.com/class/"
 
+# Provides a means of setting the forum ID from a running app.
 @app.command("/piazza-update-id")
 def update_forum_id(ack, respond, command, context):
     ack()
+
     team_id = context['team_id']
     forum_id = command['text']
-    db.upsert({'team': team_id, 'forum': forum_id }, Course.team == team_id)
+    
+    db.upsert(
+        {'workspace': team_id, 'forum': forum_id },
+        Course.workspace == team_id
+    )
+
     respond(f"Updated forum! new id is {command['text']}")
 
 
 # Listens for any message with a piazza tag in it. Piazza tags take the form
 # "@123", where the number is the id of a post on Piazza.
+# 
 # https://regex101.com/r/eMmguY/1
 @app.message(re.compile(r"@(\d+\b)"))
 def post_link(say, context, event):
+    c = db.get((Course.workspace == context["team_id"]))
+    posts_url = base_url + c["workspace"] + "/post/"
+    
     # build message contents
     text = ""
     for match in context['matches']:
