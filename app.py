56bfa86a2b32aed95ee82e11281c72e7ad67e786
Jack Lowrie <jack@4thwall.io>
2022-11-25 14:03:41 -0500

    Feature multiple workspaces (#24)
    * add app registration manifest and piazza icon
    
    * install instructions
    
    * readme formatting and clarity
    
    * add missing install instructions
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * temp contributing.md
    
    * update contributing and add venv to gitignore
    
    * bugfix: threaded message replies
    
    * install script
    
    * update readme
    
    * update readme
    
    * remove git conflict
    
    * update instructions to include install script
    
    * Feature reply in thread (#16)
    
    * Update issue templates
    
    * Delete CONTRIBUTING.md
    
    moving to .github
    
    * Revert "Update issue templates"
    
    This reverts commit 43cd34f55b71f1cf749d02f86ec9c4d6d11193c4.
    
    * Revert "Delete CONTRIBUTING.md"
    
    This reverts commit 25384edf7ce83390935e1807e2f4e50c92079cec.
    
    * Update issue templates
    
    * Create pull_request_template.md
    
    * Create pull_request_template.md
    
    * Update README.md
    
    * Update README.md
    
    * Update issue templates
    
    * Update feature_request.md
    
    * broadcast replies to unthreaded messages
    
    * delte test file
    
    * Feature update course (#21)
    
    * Develop (#19)
    
    * add app registration manifest and piazza icon
    
    * install instructions
    
    * readme formatting and clarity
    
    * add missing install instructions
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * temp contributing.md
    
    * update contributing and add venv to gitignore
    
    * bugfix: threaded message replies
    
    * install script
    
    * update readme
    
    * update readme
    
    * remove git conflict
    
    * update instructions to include install script
    
    * Feature reply in thread (#16)
    
    * Update issue templates
    
    * Delete CONTRIBUTING.md
    
    moving to .github
    
    * Revert "Update issue templates"
    
    This reverts commit 43cd34f55b71f1cf749d02f86ec9c4d6d11193c4.
    
    * Revert "Delete CONTRIBUTING.md"
    
    This reverts commit 25384edf7ce83390935e1807e2f4e50c92079cec.
    
    * Update issue templates
    
    * Create pull_request_template.md
    
    * Create pull_request_template.md
    
    * Update README.md
    
    * Update README.md
    
    * Update issue templates
    
    * Update feature_request.md
    
    * broadcast replies to unthreaded messages
    
    * delte test file
    
    * install tinydb and add slash command to upsert forum ids
    
    * remove course id from example .eng
    
    * store forum id in tinydb
    
    * move error message construction
    
    * formatting
    
    * formatting
    
    * update readme
    
    * get forum id for each message sent
    
    * add cache, rename Course -> Q (for query), move post_url building to message (#22)
    
    * update gitignore
    
    * add test message and remove dotenv

--- app.py
+++ app.py
@@ -1,24 +1,80 @@
 import os
 from dotenv import load_dotenv
+from signal import signal, SIGINT
+from sys import exit
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+import logging
 
 import re
+from tinydb import TinyDB, Query
 
 # Make the app
-load_dotenv()
+logging.basicConfig(level=logging.ERROR)
 
-app = App(token = os.environ.get("SLACK_BOT_TOKEN"))
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
 
-base_url = "https://piazza.com/class/%s" % os.environ.get("COURSE_ID")
-posts_url = f"{base_url}/post/" 
+db = TinyDB('data/db.json')
+Q = Query()
+cache = {}
+
+error = "Sorry, the forum id hasn't been set! "
+error += "You can set it via slash command:\n"
+error += "`/piazza-update-id [course_id]`\n"
+error += "You can find the course id in any url on your piazza forum. "
+error += "it'll be the long alphanumeric string."
+
+base_url = "https://piazza.com/class/"
+
+
+# Provides a means of setting the forum ID from a running app.
+@app.command("/piazza-update-id")
+def update_forum_id(ack, respond, command, context):
+    global cache
+    ack()
+
+    workspace = context['team_id']
+    forum_id = command['text']
+
+    # update in mem
+    cache[workspace] = forum_id
+
+    # update in file
+    db.upsert(
+        {'workspace': workspace, 'forum': forum_id},
+        Q.workspace == workspace
+    )
+
+    respond(f"Updated forum! new id is {forum_id}", )
+
+@app.message("thunder")
+def post_link(say, context, event, client):
+    say("lightning")
+
 
 # Listens for any message with a piazza tag in it. Piazza tags take the form
 # "@123", where the number is the id of a post on Piazza.
+# 
 # https://regex101.com/r/eMmguY/1
 @app.message(re.compile(r"@(\d+\b)"))
-def post_link(say, context, event):
+def post_link(say, context, event, client):
+    global cache
+    forum_id = cache.get(context["team_id"], None)
+
+    if forum_id == None:
+        client.chat_postEphemeral(
+            text=error,
+            channel=context["channel_id"],
+            user=context["user_id"]
+        )
+        return
+
+    posts_url = base_url + forum_id + "/post/"
+
     # build message contents
     text = ""
     for match in context['matches']:
@@ -29,17 +85,30 @@ def post_link(say, context, event):
     thread_ts = event.get("thread_ts", None)
     if thread_ts == None:
         say(
-            text = text.strip("\n"), 
-            thread_ts = event.get("ts"), 
-            reply_broadcast = True
+            text=text.strip("\n"),
+            thread_ts=event.get("ts"),
+            reply_broadcast=True
         )
     else:
         say(
-            blocks = text.strip("\n"), 
-            thread_ts = thread_ts
+            text=text.strip("\n"),
+            thread_ts=thread_ts
         )
+
+
+# exit handler
+def cleanup(signal_received, frame):
+    print("\nShutting down PiazzaBot...")
+    db.close()
+    print("goodbye!")
+    exit(0)
 
 
 # Run the app
 if __name__ == "__main__":
+    signal(SIGINT, cleanup)
+
+    for record in db:
+        cache[record['workspace']] = record['forum']
+
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
