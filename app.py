4b53bd0cb9a1687b6b8171a861e9a2a7d2e3bf95
Jack Lowrie <jack@4thwall.io>
2022-11-26 15:25:57 -0500

    Production (#26)
    * Feature multiple workspaces (#24) (#25)
    
    * add app registration manifest and piazza icon
    
    * install instructions
    
    * readme formatting and clarity
    
    * add missing install instructions
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * temp contributing.md
    
    * update contributing and add venv to gitignore
    
    * bugfix: threaded message replies
    
    * install script
    
    * update readme
    
    * update readme
    
    * remove git conflict
    
    * update instructions to include install script
    
    * Feature reply in thread (#16)
    
    * Update issue templates
    
    * Delete CONTRIBUTING.md
    
    moving to .github
    
    * Revert "Update issue templates"
    
    This reverts commit 43cd34f55b71f1cf749d02f86ec9c4d6d11193c4.
    
    * Revert "Delete CONTRIBUTING.md"
    
    This reverts commit 25384edf7ce83390935e1807e2f4e50c92079cec.
    
    * Update issue templates
    
    * Create pull_request_template.md
    
    * Create pull_request_template.md
    
    * Update README.md
    
    * Update README.md
    
    * Update issue templates
    
    * Update feature_request.md
    
    * broadcast replies to unthreaded messages
    
    * delte test file
    
    * Feature update course (#21)
    
    * Develop (#19)
    
    * add app registration manifest and piazza icon
    
    * install instructions
    
    * readme formatting and clarity
    
    * add missing install instructions
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * temp contributing.md
    
    * update contributing and add venv to gitignore
    
    * bugfix: threaded message replies
    
    * install script
    
    * update readme
    
    * update readme
    
    * remove git conflict
    
    * update instructions to include install script
    
    * Feature reply in thread (#16)
    
    * Update issue templates
    
    * Delete CONTRIBUTING.md
    
    moving to .github
    
    * Revert "Update issue templates"
    
    This reverts commit 43cd34f55b71f1cf749d02f86ec9c4d6d11193c4.
    
    * Revert "Delete CONTRIBUTING.md"
    
    This reverts commit 25384edf7ce83390935e1807e2f4e50c92079cec.
    
    * Update issue templates
    
    * Create pull_request_template.md
    
    * Create pull_request_template.md
    
    * Update README.md
    
    * Update README.md
    
    * Update issue templates
    
    * Update feature_request.md
    
    * broadcast replies to unthreaded messages
    
    * delte test file
    
    * install tinydb and add slash command to upsert forum ids
    
    * remove course id from example .eng
    
    * store forum id in tinydb
    
    * move error message construction
    
    * formatting
    
    * formatting
    
    * update readme
    
    * get forum id for each message sent
    
    * add cache, rename Course -> Q (for query), move post_url building to message (#22)
    
    * update gitignore
    
    * add test message and remove dotenv
    
    * remove unnecessary requirements
    
    * add DB setup
    
    * update DB setup
    
    * update DB setup
    
    * escape db password
    
    * try pymysql
    
    * change port back to 443 default
    
    * add scopes
    
    * change test message to only mention. trying to figure out multimessage thing
    
    * change test message to only mention. trying to figure out multimessage thing
    
    * debug command
    
    * debug command
    
    * debug command
    
    * fix logging
    
    * try a merge
    
    * warm the cache
    
    * formatting, also save the cache before quitting
    
    * update logging level
    
    * whoops

--- app.py
+++ app.py
@@ -1,26 +1,95 @@
 import os
-from dotenv import load_dotenv
 from signal import signal, SIGINT
 from sys import exit
 
 from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-import logging
+from slack_bolt.oauth.oauth_settings import OAuthSettings
+from slack_sdk.oauth.installation_store.sqlalchemy import SQLAlchemyInstallationStore
+from slack_sdk.oauth.state_store.sqlalchemy import SQLAlchemyOAuthStateStore
+
+import sqlalchemy
+from sqlalchemy.engine import Engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from urllib import parse
 
 import re
-from tinydb import TinyDB, Query
 
-# Make the app
-logging.basicConfig(level=logging.ERROR)
+import logging
+logger = logging.getLogger(__name__)
 
-app = App(
-    token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+# Set up DB
+db_host, db_user, db_pass, db_name = (
+    os.environ.get("DB_HOST"),
+    os.environ.get("DB_USER"),
+    parse.quote_plus(os.environ.get("DB_PASS")),
+    os.environ.get("DB_NAME")
+)
+client_id, client_secret, signing_secret = (
+    os.environ["SLACK_CLIENT_ID"],
+    os.environ["SLACK_CLIENT_SECRET"],
+    os.environ["SLACK_SIGNING_SECRET"],
+)
+connection = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}"
+engine: Engine = sqlalchemy.create_engine(connection)
+
+# Set up Oauth backend
+installation_store = SQLAlchemyInstallationStore(
+    client_id=client_id,
+    engine=engine,
+    logger=logger
+)
+oauth_state_store = SQLAlchemyOAuthStateStore(
+    expiration_seconds=120,
+    engine=engine,
+    logger=logger
 )
 
-db = TinyDB('data/db.json')
-Q = Query()
-cache = {}
+try:
+    engine.execute("select count(*) from slack_bots")
+except Exception as e:
+    installation_store.metadata.create_all(engine)
+    oauth_state_store.metadata.create_all(engine)
+
+# Set up custom table
+Base = declarative_base()
+
+
+class Course(Base):
+    __tablename__ = 'courses'
+    __table_args__ = {'schema': db_name}
+
+    workspace = Column("workspace", String(32), nullable=False, primary_key=True)
+    forum = Column("forum", String(32), nullable=False)
+
+    def __repr__(self):
+        return "Course({0}, {1})".format(self.workspace, self.forum)
+
+
+Base.metadata.create_all(engine)
+
+# Set up app
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=signing_secret,
+    installation_store=installation_store,
+    oauth_settings=OAuthSettings(
+        client_id=client_id,
+        client_secret=client_secret,
+        state_store=oauth_state_store,
+        scopes=[
+            "chat:write.public",
+            "chat:write",
+            "channels:history",
+            "groups:history",
+            "im:history",
+            "mpim:history",
+            "commands"
+        ]
+    )
+)
 
 error = "Sorry, the forum id hasn't been set! "
 error += "You can set it via slash command:\n"
@@ -28,44 +97,43 @@ error += "`/piazza-update-id [course_id]`\n"
 error += "You can find the course id in any url on your piazza forum. "
 error += "it'll be the long alphanumeric string."
 
+cache = {}
 base_url = "https://piazza.com/class/"
 
 
 # Provides a means of setting the forum ID from a running app.
 @app.command("/piazza-update-id")
 def update_forum_id(ack, respond, command, context):
+    logging.warning("in command handler.")
     global cache
     ack()
 
-    workspace = context['team_id']
+    workspace_id = context['team_id']
     forum_id = command['text']
 
     # update in mem
-    cache[workspace] = forum_id
-
-    # update in file
-    db.upsert(
-        {'workspace': workspace, 'forum': forum_id},
-        Q.workspace == workspace
-    )
+    cache[workspace_id] = forum_id
+    logging.info(f"after cache insert. cache[{workspace_id}] should be {forum_id}, is {cache[workspace_id]}")
+    c = Course(workspace=workspace_id, forum=forum_id)
+    with Session(engine) as session:
+        logging.info(f"in session thing. course is: {c}")
+        session.merge(c)
+        session.commit()
+        logging.info("after commit")
 
     respond(f"Updated forum! new id is {forum_id}", )
-
-@app.message("thunder")
-def post_link(say, context, event, client):
-    say("lightning")
 
 
 # Listens for any message with a piazza tag in it. Piazza tags take the form
 # "@123", where the number is the id of a post on Piazza.
-# 
+#
 # https://regex101.com/r/eMmguY/1
 @app.message(re.compile(r"@(\d+\b)"))
 def post_link(say, context, event, client):
     global cache
     forum_id = cache.get(context["team_id"], None)
 
-    if forum_id == None:
+    if forum_id is None:
         client.chat_postEphemeral(
             text=error,
             channel=context["channel_id"],
@@ -83,7 +151,7 @@ def post_link(say, context, event, client):
 
     # send the message
     thread_ts = event.get("thread_ts", None)
-    if thread_ts == None:
+    if thread_ts is None:
         say(
             text=text.strip("\n"),
             thread_ts=event.get("ts"),
@@ -98,9 +166,17 @@ def post_link(say, context, event, client):
 
 # exit handler
 def cleanup(signal_received, frame):
-    print("\nShutting down PiazzaBot...")
-    db.close()
-    print("goodbye!")
+    logging.info("Shutting down PiazzaBot...")
+
+    global cache
+    global engine
+    with Session(engine) as session:
+        for workspace in cache:
+            course = Course(workspace=workspace, forum=cache[workspace])
+            session.merge(course)
+        session.commit()
+
+    logging.info("goodbye!")
     exit(0)
 
 
@@ -108,7 +184,9 @@ def cleanup(signal_received, frame):
 if __name__ == "__main__":
     signal(SIGINT, cleanup)
 
-    for record in db:
-        cache[record['workspace']] = record['forum']
+    with Session(engine) as session:
+        courses = session.query(Course)
+        for course in courses:
+            cache[course.workspace] = course.forum
 
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    app.start(port=int(os.environ.get("PORT", 443)))
