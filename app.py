fbb54fcde044842bf9d445b698a960c35fd3d488
Jack Lowrie <jack@4thwall.io>
2022-11-26 13:05:37 -0500

    debug command

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import re
 
 import logging
 
-logging.basicConfig(level=logging.ERROR)
+# logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
 
 # Set up DB
@@ -105,6 +105,7 @@ base_url = "https://piazza.com/class/"
 # Provides a means of setting the forum ID from a running app.
 @app.command("/piazza-update-id")
 def update_forum_id(ack, respond, command, context):
+    print("in command handler.")
     global cache
     ack()
 
@@ -113,11 +114,13 @@ def update_forum_id(ack, respond, command, context):
 
     # update in mem
     cache[workspace_id] = forum_id
-
+    print(f"after cache insert. cache[{workspace_id}] should be {forum_id}, is {cache[workspace_id]}")
     c = Course(workspace=workspace_id, forum=forum_id)
     with Session(engine) as session:
+        print("in session thing")
         session.add(c)
         session.commit()
+        print("after commit")
 
     respond(f"Updated forum! new id is {forum_id}", )
 
